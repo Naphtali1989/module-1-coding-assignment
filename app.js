7637756c343e0071584bd797b1ab96b1f5b1fe03
Naphtali Rubin <naphtali.r.89@gmail.com>
2021-02-16 20:17:17 +0200

    finished the task

--- app.js
+++ app.js
@@ -7,11 +7,23 @@
     FoodController.$inject = ['$scope', '$filter']
 
     function FoodController($scope, $filter) {
-        $scope.name = 'Naphtali';
+        $scope.food = '';
+        $scope.foodAmmount = 0;
 
         $scope.upper = () => {
             var upCase = $filter('uppercase');
             $scope.name = upCase($scope.name);
+        }
+
+        $scope.checkFoodAmount = () => {
+            const foods = $scope.food.split(',').filter(food => food)
+            $scope.foodAmmount = foods.length
+        }
+
+        $scope.showMsg = () => {
+            if ($scope.foodAmmount > 3) return 'Too much!'
+            else if ($scope.foodAmmount > 0) return 'Enjoy!'
+            return ''
         }
     }
 })();
