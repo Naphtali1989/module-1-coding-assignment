77eeaeaf91c9b477d2ce7fd6ccb8f652fa4880ad
Naphtali Rubin <naphtali.r.89@gmail.com>
2021-02-16 20:52:35 +0200

    changed functinality of default message :)

--- app.js
+++ app.js
@@ -9,21 +9,24 @@
     function FoodController($scope, $filter) {
         $scope.food = '';
         $scope.foodAmmount = 0;
+        $scope.isSubmitted = false
 
         $scope.checkFoodAmount = () => {
             const foods = $scope.food.split(',').filter(food => food.trim().length > 0)
             $scope.foodAmmount = foods.length
+
+            $scope.isSubmitted = true
         }
 
         $scope.showMsg = () => {
             if ($scope.foodAmmount > 3) return 'Too much!'
             else if ($scope.foodAmmount > 0) return 'Enjoy!'
-            return 'Please enter data first'
+            else if ($scope.isSubmitted) return 'Please enter data first'
         }
 
         $scope.msgStyle = () => {
             if ($scope.foodAmmount > 0) return { color: 'green', border: '1px solid green' }
-            return { color: 'red', border: '1px solid red' }
+            else if ($scope.isSubmitted) return { color: 'red', border: '1px solid red' }
         }
 
     }
